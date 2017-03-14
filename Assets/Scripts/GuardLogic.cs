1920d40030a09e3bbfed338a244e820bf2f7b1bc
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-03-14 14:10:49 -0700

    Adds Guard logic

--- Assets/Scripts/GuardLogic.cs
+++ Assets/Scripts/GuardLogic.cs
@@ -3,5 +3,31 @@ using System.Collections;
 
 public class GuardLogic : MonoBehaviour {
 
+	public Transform sightStart, sightEnd;
+
+	public bool spotted = false;
+
+	void Update()
+	{
+
+		Raycasting();
+		Behaviors();
+	}
+
+	void Raycasting() 
+	{
+		// rays are invisible, which is why we use Debug.DrawLine
+		Debug.DrawLine(sightStart.position, sightEnd.position, Color.green);
+		spotted = Physics2D.Linecast(sightStart.position, sightEnd.position, 1 << LayerMask.NameToLayer("Player"));
+		 // linecast common for 2D
+		// LayerMask b.s. ensures that spotted only goes true if guard sees player object.
+
+
+	}
+
+	void Behaviors() 
+	{
+		
+	}
 
 }
