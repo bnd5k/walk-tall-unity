3be03a881f268b7bde23873acfa4a4c82c392959
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-03-14 13:18:44 -0700

    Player movements

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,5 +3,21 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour{
 
+	void Update() {
+		Movement();
+	}
 
+	void Movement() {
+		if(Input.GetKey(KeyCode.D)) 
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		}		
+		if(Input.GetKey(KeyCode.A)) 
+		{
+			transform.Translate(Vector2.right * 4f * Time.deltaTime);
+			transform.localRotation =  Quaternion.Euler(0, 180, 0);
+
+		}
+	}
 }
