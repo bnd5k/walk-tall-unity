6cc4663edd082aa9cc38f75259cb1c00ddaf7e67
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-03-15 09:16:19 -0700

    Adds ability to destroy guards when in range

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,8 +3,38 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour{
 
+	public bool interact = false;
+	public Transform lineStart, lineEnd;
+
+	RaycastHit2D whatIHit;
+
+
 	void Update() {
 		Movement();
+		Raycasting();
+	}
+
+	void Raycasting()
+	{
+		Debug.DrawLine(lineStart.position, lineEnd.position, Color.green);
+
+		if (Physics2D.Linecast(lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Guard")))
+		{
+			Debug.Log("farts");
+
+			whatIHit = Physics2D.Linecast(lineStart.position, lineEnd.position, 1 << LayerMask.NameToLayer("Guard"));
+			interact = true;
+
+		}
+		else 
+		{
+			Debug.Log("poop");
+			interact = false;
+		}
+		if (Input.GetKeyDown(KeyCode.E) && interact == true) 
+		{
+			Destroy((whatIHit.collider.gameObject));
+		}
 	}
 
 	void Movement() {
